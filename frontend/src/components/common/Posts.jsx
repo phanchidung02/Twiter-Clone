2d94dcfac15c51293130ac5d4bf8a3f750d4258f
phanchidung02 <phanchidung3107@gmail.com>
2024-05-18 16:03:08 +0700

    Done Posts Feature Part 1

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,25 +1,57 @@
 import Post from './Post';
 import PostSkeleton from '../skeletons/PostSkeleton';
-import { POSTS } from '../../utils/db/dummy';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useEffect } from 'react';
 
-const Posts = () => {
-  const isLoading = false;
+const Posts = ({ feedType }) => {
+  const postEndPoint = feedType === 'forYou' ? 'all' : 'following';
 
+  const {
+    data: post,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:5000/api/posts/${postEndPoint}`,
+          { withCredentials: true }
+        );
+        if (res.status !== 200) throw new Error('Something went wrong');
+
+        const data = res.data;
+        console.log(data);
+        return data.data;
+      } catch (err) {
+        throw err;
+      }
+    },
+  });
+
+  useEffect(
+    function () {
+      refetch();
+    },
+    [feedType, refetch]
+  );
   return (
     <>
-      {isLoading && (
+      {(isLoading || isRefetching) && (
         <div className="flex flex-col justify-center">
           <PostSkeleton />
           <PostSkeleton />
           <PostSkeleton />
         </div>
       )}
-      {!isLoading && POSTS?.length === 0 && (
+      {(!isLoading || !isRefetching) && post?.length === 0 && (
         <p className="text-center my-4">No posts in this tab. Switch 👻</p>
       )}
-      {!isLoading && POSTS && (
+      {!isLoading && post && (
         <div>
-          {POSTS.map((post) => (
+          {post.map((post) => (
             <Post key={post._id} post={post} />
           ))}
         </div>
